367813ee3ed6075c3b9e694955cbfae26a901547
Guest User <Guest@harinaths-macbook-pro.local>
2016-10-07 13:50:02 -0700

    Initial Commit

---
+++ CS646_Assignment3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS646_Assignment3
+//
+//  Created by Guest User on 10/7/16.
+//  Copyright © 2016 Mounika Eluri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
